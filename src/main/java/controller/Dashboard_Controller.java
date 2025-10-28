3b4727348329d744fcd576427cdb90b649bb821a
tuanank5 <ADMIN@LAPTOP-HK5N2JJO>
2025-10-28 08:36:38 +0700

    c

--- src/main/java/controller/Dashboard_Controller.java
+++ src/main/java/controller/Dashboard_Controller.java
@@ -3,11 +3,5 @@ package controller;
 import entity.TaiKhoan;
 
 public class Dashboard_Controller {
-	private TaiKhoan taiKhoan;
-
-	public void setThongTin(TaiKhoan taiKhoan) {
-        this.taiKhoan = taiKhoan;
-        String hoTen = taiKhoan.getNhanVien().getTenNV();
-//        txtThongTinNhanVien.setText(hoTen);
-    }
+	
 }
