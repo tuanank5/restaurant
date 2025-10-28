00c353a2f0082b7c72a8c88abc76bec69fae80ef
thao <tn364471@gmail.com>
2025-10-28 20:56:16 +0700

    commit

--- src/main/java/dao/impl/Ban_DAOImpl.java
+++ src/main/java/dao/impl/Ban_DAOImpl.java
@@ -1,8 +1,18 @@
 package dao.impl;
 
-import dao.Ban_DAO;
-import entity.Ban;
+import dao.QuanLyBan_DAO;
+import entity.QuanLyBan;
 
-public class Ban_DAOImpl extends Entity_DAOImpl<Ban> implements Ban_DAO{
+public class Ban_DAOImpl extends Entity_DAOImpl<QuanLyBan> implements QuanLyBan_DAO {
+
+	 private final Class<QuanLyBan> entityClass = QuanLyBan.class;
+
+	    public Ban_DAOImpl() {
+	        // Không gọi super
+	    }
+
+	    public Class<QuanLyBan> getEntityClass() {
+	        return entityClass;
+	    }
 
 }
