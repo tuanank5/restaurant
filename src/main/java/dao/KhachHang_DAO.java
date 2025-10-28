520695083f0d6381a30aba7c8d73fa7d901ccd20
thao <tn364471@gmail.com>
2025-10-28 13:58:31 +0700

    commit

---
+++ src/main/java/dao/KhachHang_DAO.java
@@ -0,0 +1,7 @@
+package dao;
+
+import entity.KhachHang;
+
+public interface KhachHang_DAO extends Entity_DAO<KhachHang> {
+
+}
---
+++ src/main/java/dao/impl/KhachHang_DAOlmpl.java
@@ -0,0 +1,8 @@
+package dao.impl;
+
+import dao.KhachHang_DAO;
+import entity.KhachHang;
+
+public class KhachHang_DAOlmpl extends Entity_DAOImpl<KhachHang> implements KhachHang_DAO {
+
+}
