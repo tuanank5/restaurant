bfa8fb8dfeb035e67f63ade1fe5affb5a7606576
thao <tn364471@gmail.com>
2025-10-28 14:19:53 +0700

    commit

--- sql/InsertSQL.sql
+++ sql/InsertSQL.sql
@@ -11,3 +11,16 @@ VALUES
 ('TK001', 'sa', '1', '2023-10-01', '2023-10-02', '2023-10-03', 'NV001'),
 ('TK002', 'nv', '1', '2023-10-01', '2023-10-02', '2023-10-03', 'NV002'),
 ('TK003', 'userC', 'passwordC', '2023-10-01', '2023-10-02', '2023-10-03', 'NV003');
+
+
+--Chèn dữ liệu vào bảng HangKhachHang
+INSERT INTO HangKhachHang (maHang, tenHang, diemHang, giamGia, moTa)
+VALUES
+('H1', N'Thường', 0, 0, N'Hạng thường - không giảm giá'),
+('H2', N'Bạc', 100, 5, N'Hạng Bạc - giảm 5%'),
+('H3', N'Vàng', 300, 10, N'Hạng Vàng - giảm 10%'),
+('H4', N'Kim Cương', 600, 15, N'Hạng Kim Cương - giảm 15%');
+
+--Chèn dữ liệu vào bảng HangKhac
+INSERT INTO KhachHang (maKH, tenKH, sdt, diaChi, maHang, diemTichLuy)
+VALUES ('KH001', N'Nguyễn Văn A', '0901111111', N'123 Lê Lợi, TP.HCM', 'H1', 50);
