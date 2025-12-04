768a83ba850995b3822a3143f591ff057f358e95
lamthanhdat1305-maker <lamthanhdat1305@gmail.com>
2025-12-04 23:02:55 +0700

    Commit-Dat

--- src/main/java/controller/DatBan/DonDatBan_Controller.java
+++ src/main/java/controller/DatBan/DonDatBan_Controller.java
@@ -1,12 +1,23 @@
 package controller.DatBan;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
+import config.RestaurantApplication;
 import controller.Menu.MenuNV_Controller;
+import dao.DonDatBan_DAO;
+import dao.KhachHang_DAO;
 import entity.Ban;
 import entity.DonDatBan;
+import entity.HangKhachHang;
+import entity.KhachHang;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +26,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class DonDatBan_Controller implements Initializable{
 	@FXML
@@ -22,27 +34,24 @@ public class DonDatBan_Controller implements Initializable{
 
     @FXML
     private DatePicker dpNgayDatBan;
-    
-    @FXML
-    private TableColumn<?, ?> tblDatMon;
 
     @FXML
-    private TableColumn<?, ?> tblGioDen;
+    private TableColumn<DonDatBan, String> tblGioDen;
 
     @FXML
-    private TableColumn<?, ?> tblKhachHang;
+    private TableColumn<DonDatBan, String> tblKhachHang;
 
     @FXML
-    private TableColumn<?, ?> tblSoBan;
+    private TableColumn<DonDatBan, String> tblSoBan;
 
     @FXML
-    private TableColumn<?, ?> tblSoNguoi;
+    private TableColumn<DonDatBan, String> tblSoNguoi;
 
     @FXML
-    private TableColumn<?, ?> tblTienCoc;
+    private TableColumn<DonDatBan, String> tblTienCoc;
 
     @FXML
-    private TableColumn<?, ?> tblTrangThai;
+    private TableColumn<DonDatBan, String> tblTrangThai;
 
     @FXML
     private TableView<DonDatBan> tblView;
@@ -53,10 +62,17 @@ public class DonDatBan_Controller implements Initializable{
     @FXML
     private TextField txtTongDonDat;
     
+    private ObservableList<DonDatBan> danhSachDonDatBan = FXCollections.observableArrayList();
+    private List<DonDatBan> danhSachDonDatBanDB;
+    private final int LIMIT = 15;
+    private String status = "all";
+    
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		khoiTaoComboBoxes();
+		setValueTable();
+        loadData();
 	}
     
     @FXML
@@ -89,4 +105,48 @@ public class DonDatBan_Controller implements Initializable{
         cmbTrangThai.getItems().addAll("Tất cả","Đã Nhận Bàn","Chưa Nhận Bàn");
         cmbTrangThai.getSelectionModel().select("Tất cả");
     }
+    
+    private void loadData() {
+        Map<String, Object> filter = new HashMap<>();
+
+        danhSachDonDatBanDB = RestaurantApplication.getInstance()
+                .getDatabaseContext()
+                .newEntity_DAO(DonDatBan_DAO.class)
+                .getDanhSach(DonDatBan.class, filter);
+
+        danhSachDonDatBan.clear();
+        danhSachDonDatBan.addAll(danhSachDonDatBanDB);
+
+        tblView.setItems(danhSachDonDatBan);
+    }
+
+    private void loadData(List<DonDatBan> list) {
+        danhSachDonDatBan.clear();
+        danhSachDonDatBan.addAll(list);
+        tblView.setItems(danhSachDonDatBan);
+    }
+
+    private void setValueTable() {
+        tblKhachHang.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().getKhachHang() 
+                		!= null ?cellData.getValue().getKhachHang().getTenKH() : "")
+        );
+
+        tblSoBan.setCellValueFactory(cellData ->new SimpleStringProperty(cellData.getValue().getBan() 
+        		!= null ?cellData.getValue().getBan().getMaBan() : "")
+        );
+
+        tblSoNguoi.setCellValueFactory(cellData ->new SimpleStringProperty(String.valueOf(cellData.getValue().getSoLuong()))
+        );
+
+        tblGioDen.setCellValueFactory(cellData ->new SimpleStringProperty(cellData.getValue().getGioBatDau() 
+        		!= null ?cellData.getValue().getGioBatDau().toString() : "")
+        );
+
+        tblTienCoc.setCellValueFactory(cellData ->new SimpleStringProperty("0")); // nếu có tiền cọc thực tế thì thay
+
+        tblTrangThai.setCellValueFactory(cellData ->new SimpleStringProperty(cellData.getValue().getTrangThai() != 
+        		null ?cellData.getValue().getTrangThai() : ""));
+    }
+
 }
