6bb6549010dc737aebb6abdd3fca3515d1a7254c
Admin <admin@restaurant.local>
2025-12-12 18:10:39 +0700

    c

--- src/main/java/controller/DatBan/AThanhToan_Controller.java
+++ src/main/java/controller/DatBan/AThanhToan_Controller.java
@@ -2,11 +2,13 @@ package controller.DatBan;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import config.RestaurantApplication;
 import controller.Menu.MenuNV_Controller;
@@ -14,17 +16,21 @@ import dao.Ban_DAO;
 import dao.ChiTietHoaDon_DAO;
 import dao.DonDatBan_DAO;
 import dao.HoaDon_DAO;
+import dao.KhuyenMai_DAO;
 import dao.MonAn_DAO;
 import dao.NhanVien_DAO;
 import dao.impl.Ban_DAOImpl;
 import dao.impl.ChiTietHoaDon_DAOImpl;
 import dao.impl.DonDatBan_DAOImpl;
 import dao.impl.HoaDon_DAOImpl;
+import dao.impl.KhuyenMai_DAOImpl;
 import dao.impl.MonAn_DAOImpl;
 import entity.Ban;
 import entity.ChiTietHoaDon;
 import entity.DonDatBan;
+import entity.HangKhachHang;
 import entity.HoaDon;
+import entity.KhuyenMai;
 import entity.MonAn;
 import entity.NhanVien;
 import javafx.beans.property.ReadOnlyObjectWrapper;
@@ -106,31 +112,103 @@ public class AThanhToan_Controller {
     private ChiTietHoaDon_DAO cthdDAO = new ChiTietHoaDon_DAOImpl();
     private MonAn_DAO monAnDAO = new MonAn_DAOImpl();
     private DonDatBan_DAO donDatBanDao = new DonDatBan_DAOImpl();
+    
+    private KhuyenMai_DAO kmDAO = new KhuyenMai_DAOImpl();
 
     // --- Danh sách và trạng thái ---
     private List<HoaDon> dsHoaDon = new ArrayList<>();
-    private List<Ban> dsBan = new ArrayList<Ban>();
-    private List<DonDatBan> dsDDB = new ArrayList<DonDatBan>();
-    private Ban hoaDonDangChon = null;
-    private Button buttonHoaDonDangChonUI = null;
-    private String maHoaDonDangChon;
-    private boolean isFromSearch = false;
+    private List<KhuyenMai> dsKM = new ArrayList<>();
     
     private ObservableList<ChiTietHoaDon> dsCTHD = FXCollections.observableArrayList();
     private List<ChiTietHoaDon> dsCTHD_DB;
     
-    private Map<MonAn, Integer> dsMonAnDat = new LinkedHashMap<>();
+    private HoaDon hoaDonHienTai;
 
     @FXML
     public void initialize() {
     	String maHD = MenuNV_Controller.instance.aBanHienTai_HD.getMaHoaDon();
     	dsCTHD_DB = cthdDAO.getChiTietTheoMaHoaDon(maHD);
+    	
+    	double tongThanhTien = 0;
+    	
+    	for (ChiTietHoaDon cthd : dsCTHD_DB) {
+    		tongThanhTien += cthd.getMonAn().getDonGia() * cthd.getSoLuong();
+    	}
     
     	setValueTbl();
         loadData(dsCTHD_DB);
+        
+        dsHoaDon = hoaDonDAO.getDanhSach("HoaDon.list", HoaDon.class);
+        for (HoaDon hd : dsHoaDon) {
+        	if (hd.getMaHoaDon().equals(maHD)) {
+        		hoaDonHienTai = hd;
+        	}
+        }
+        
+        txtSDT.setText(hoaDonHienTai.getKhachHang().getSdt());
+        txtKH.setText(hoaDonHienTai.getKhachHang().getTenKH());
+        txtSL.setText(hoaDonHienTai.getDonDatBan().getSoLuong() + "");
+        txtBan.setText(hoaDonHienTai.getDonDatBan().getBan().getMaBan());
+        
+        loadCmbKM();
+        
+        lblThanhTien.setText(formatTienVN(tongThanhTien));
     }
     
-    private void setValueTbl() {
+    private void loadCmbKM() {
+    	List<KhuyenMai> dsKM = kmDAO.getDanhSach("KhuyenMai.list", KhuyenMai.class);
+    	dsKM = dsKM.stream()
+                .sorted(Comparator.comparingDouble(KhuyenMai::getPhanTramGiamGia).reversed())
+                .collect(Collectors.toList());
+    	for(KhuyenMai km : dsKM) {
+            cmbKM.getItems().add(km.getPhanTramGiamGia() + "%" + " - " + km.getTenKM());
+        }
+    	
+    	cmbKM.setValue(cmbKM.getItems().get(0));
+    	
+    	cmbKM.setOnAction(e -> capNhatTongTien());
+	}
+    
+    private void capNhatTongTien() {
+//        double tongTruocVAT = dsMonAnDat.entrySet().stream()
+//            .mapToDouble(e -> e.getKey().getDonGia() * e.getValue())
+//            .sum();
+//        double tienGiam = 0;
+//        KhuyenMai km = cmbKM.getValue();
+//        
+//        if (km != null) {
+//            switch (km.getLoaiKM()) {
+//
+//                case "Khuyến mãi trên tổng hóa đơn":
+//                    tienGiam = tongTruocVAT * km.getPhanTramGiamGia() / 100.0;
+//                    break;
+//
+//                case "Khuyến mãi món ăn":
+//                    tienGiam = tinhGiamGiaTheoMon(km);
+//                    break;
+//
+//                case "Ưu đãi cho khách hàng Kim Cương":
+//                    KhachHang kh = khachHangDAO.timTheoMa(txtMaKH.getText());
+//                    if (kh != null && kh.getHangKhachHang() != null &&
+//                            kh.getHangKhachHang().getTenHang().equalsIgnoreCase("Hạng Kim Cương")) {
+//                        tienGiam = tongTruocVAT * km.getPhanTramGiamGia() / 100.0;
+//                    }
+//                    break;
+//            }
+//        }
+//        double tongSauGiam = tongTruocVAT - tienGiam;
+//        if (tongSauGiam < 0) tongSauGiam = 0;
+//        
+//        double tienVAT = tongTruocVAT * Vat_Rate;
+//        double tongSauVAT = tongSauGiam + tienVAT;
+//
+//        lblVat.setText(dinhDangTien(tienVAT));
+//        lblTongTien.setText(dinhDangTien(tongTruocVAT));
+//        lblTienGiam.setText(dinhDangTien(tienGiam));
+//        lblTongTienVAT.setText(dinhDangTien(tongSauVAT));
+    }
+
+	private void setValueTbl() {
     	colSTT.setCellValueFactory(col -> {
             int index = tblDS.getItems().indexOf(col.getValue());
             return new ReadOnlyObjectWrapper<>(index >= 0 ? index + 1 : 0);
@@ -173,9 +251,6 @@ public class AThanhToan_Controller {
     private String formatTienVN(double tien) {
 		NumberFormat nf = NumberFormat.getInstance(new Locale("vi", "VN"));
 	    nf.setGroupingUsed(true);
-	    //nf.setMaximumFractionDigits(0); // không hiển thị phần thập phân
-	    nf.setMinimumFractionDigits(3); // hiển thị ít nhất 3 số sau dấu thập phân
-	    nf.setMaximumFractionDigits(3);
 	    return nf.format(tien) + " VND";
 	}
 }
