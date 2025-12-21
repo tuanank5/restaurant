eaf8789fc55e8aa50571edcaa109ce6f8ab864dc
Admin <admin@restaurant.local>
2025-12-21 23:22:43 +0700

    c

---
+++ src/main/java/controller/Help/HelpScroll.java
@@ -0,0 +1,30 @@
+package controller.Help;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+
+public class HelpScroll {
+	@FXML
+	public ScrollPane pane_Dashboard;
+	
+	@FXML
+    public void initialize(){
+        loadTabContent(pane_Dashboard, "/view/fxml/Help/a.fxml");
+    }
+
+    private void loadTabContent(ScrollPane pane, String fxmlFile){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Node content = loader.load();
+            pane.setContent(content);
+            pane.setFitToHeight(true);
+            pane.setFitToWidth(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
